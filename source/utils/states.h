8479e6ecb03edaa1453ad7ff6cf8c6efbe2e7e93
KristofferMoevik <kristoffersmovik@live.no>
2022-03-15 12:09:22 +0100

    started doxygening

--- source/utils/states.h
+++ source/utils/states.h
@@ -1,6 +1,10 @@
 #pragma once
 #include <inttypes.h>
 
+/**
+* @file
+* @brief A library for the finite state machine in main.c
+*/
 
 enum Current_pos {
     UNDEFINED = -10,
@@ -26,10 +30,43 @@ enum States {
     OPEN_DOORS_STOP = 9,
 };
 
+
+/**
+* @brief Drives the elevator to the floor underneat when the elevator starts
+*
+* @param[in] current_pos value of the position of the elevator. Has values [10, 15, 20, 25, 30, 35, 40]
+* @param[in, out] motor_direction value of the motor_direction. Has values [-1, 0, 1]
+* @param[in, out] STATE state of the finite state machine. Has value [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+*
+* @return @p STATE state of the finite state machine. Has value [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+*/
 int64_t states_INIT_STATE(int64_t current_pos, int64_t *motor_direction, int64_t STATE);
 
+/**
+* @brief Drives the elevator to the floor underneat when the elevator starts
+*
+* @param[in] ordered_store value of the ordered store.
+* @param[in] current_pos value of the position of the elevator. Has values [10, 15, 20, 25, 30, 35, 40]
+* @param[in, out] STATE state of the finite state machine. Has value [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+*
+* @return @p STATE state of the finite state machine. Has value [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+*/
 int64_t states_IDLE(int64_t ordered_store, int64_t current_pos, int64_t STATE);
 
+/**
+* @brief Drives the elevator to the floor underneat when the elevator starts
+*
+* @param[in] ordered_store value of the ordered store.
+* @param[in] current_pos value of the position of the elevator. Has values [10, 15, 20, 25, 30, 35, 40]
+* @param[in] UP_2 value of when store 2 ordered up. Has value 0 if no order, has time stamp else
+* @param[in] current_pos value of the position of the elevator. Has values [10, 15, 20, 25, 30, 35, 40]
+* @param[in] current_pos value of the position of the elevator. Has values [10, 15, 20, 25, 30, 35, 40]
+* @param[in] current_pos value of the position of the elevator. Has values [10, 15, 20, 25, 30, 35, 40]
+* @param[in] current_pos value of the position of the elevator. Has values [10, 15, 20, 25, 30, 35, 40]
+* @param[in, out] STATE state of the finite state machine. Has value [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+*
+* @return @p STATE state of the finite state machine. Has value [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+*/
 int64_t states_GO_UP(int64_t ordered_store, int64_t current_pos, int64_t UP_2, int64_t CAB_2, int64_t UP_3, int64_t CAB_3, int64_t last_pos, int64_t *motor_direction, int64_t STATE);
 
 int64_t states_GO_DOWN(int64_t ordered_store, int64_t current_pos, int64_t DOWN_2, int64_t CAB_2, int64_t DOWN_3, int64_t CAB_3, int64_t last_pos, int64_t *motor_direction, int64_t STATE);
